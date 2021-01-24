e5cf6fdb83c98ac56f3e25ad01219ab25396529f
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-01-23 23:55:41 -0600

    added functionality to display first question in array on start button and last four questions on submit button, need to fix so it only displays one at a time

--- script.js
+++ script.js
@@ -21,49 +21,95 @@ startBtn.addEventListener("click", function(){
 
 var questionArray = [
     {
-        "Question Text": "put question text here",
-        "Answer One": "put answer one answer here", 
-        "Answer Two": "put answer two answer here", 
-        "Answer Three": "put answer three answer here", 
-        "Answer Four": "put answer four answer here",
-        "Correct Answer": "figure out how to denote corrent answer here"
+        "Q": "put question text here",
+        "A1": "put answer one answer here", 
+        "A2": "put answer two answer here", 
+        "A3": "put answer three answer here", 
+        "A4": "put answer four answer here",
+        "Correct": "figure out how to denote corrent answer here"
     },
     {
-        "Question Text": "put question text here",
-        "Answer One": "put answer one answer here",
-        "Answer Two": "put answer two answer here", 
-        "Answer Three": "put answer three answer here", 
-        "Answer Four": "put answer four answer here",
-        "Correct Answer": "figure out how to denote corrent answer here"
+        "Q": "put question text here",
+        "A1": "put answer one answer here", 
+        "A2": "put answer two answer here", 
+        "A3": "put answer three answer here", 
+        "A4": "put answer four answer here",
+        "Correct": "figure out how to denote corrent answer here"
     },
     {
-        "Question Text": "put question text here",
-        "Answer One": "put answer one answer here",
-        "Answer Two": "put answer two answer here", 
-        "Answer Three": "put answer three answer here", 
-        "Answer Four": "put answer four answer here",
-        "Correct Answer": "figure out how to denote corrent answer here"
+        "Q": "put question text here",
+        "A1": "put answer one answer here", 
+        "A2": "put answer two answer here", 
+        "A3": "put answer three answer here", 
+        "A4": "put answer four answer here",
+        "Correct": "figure out how to denote corrent answer here"
     },
     {
-        "Question Text": "put question text here",
-        "Answer One": "put answer one answer here",
-        "Answer Two": "put answer two answer here", 
-        "Answer Three": "put answer three answer here", 
-        "Answer Four": "put answer four answer here",
-        "Correct Answer": "figure out how to denote corrent answer here"
+        "Q": "put question text here",
+        "A1": "put answer one answer here", 
+        "A2": "put answer two answer here", 
+        "A3": "put answer three answer here", 
+        "A4": "put answer four answer here",
+        "Correct": "figure out how to denote corrent answer here"
     },
     {
-        "Question Text": "put question text here",
-        "Answer One": "put answer one answer here",
-        "Answer Two": "put answer two answer here", 
-        "Answer Three": "put answer three answer here", 
-        "Answer Four": "put answer four answer here",
-        "Correct Answer": "figure out how to denote corrent answer here"
+        "Q": "put question text here",
+        "A1": "put answer one answer here", 
+        "A2": "put answer two answer here", 
+        "A3": "put answer three answer here", 
+        "A4": "put answer four answer here",
+        "Correct": "figure out how to denote corrent answer here"
     },
 
 ]
 
 // function to display questions to page - first question displayed on start button click event, next four questions displayed on submit button
+
+var quesContainer = document.getElementById("questions")
+var questionDiv = document.getElementById("question-text")
+var a1Div = document.getElementById("A1");
+var a2Div = document.getElementById("A2");
+var a3Div = document.getElementById("A3");
+var a4Div = document.getElementById("A4");
+
+startBtn.addEventListener("click", function(){
+    var questionText = document.createTextNode("Question: " + questionArray[0].Q)
+    questionDiv.appendChild(questionText)
+    var a1Text = document.createTextNode("a1: " + questionArray[0].A1);
+    a1Div.appendChild(a1Text);
+    var a2Text = document.createTextNode("a2: " + questionArray[0].A2);
+    a2Div.appendChild(a2Text);
+    var a3Text = document.createTextNode("a3: " + questionArray[0].A3);
+    a3Div.appendChild(a3Text);
+    var a4Text = document.createTextNode("a4: " + questionArray[0].A4);
+    a4Div.appendChild(a4Text);
+})
+
+var submitBtn = document.getElementById("submit")
+
+submitBtn.addEventListener("click", function(){
+    
+    questionDiv.textContent = "";
+    a1Div.textContent = "";
+    a2Div.textContent = "";
+    a3Div.textContent = "";
+    a4Div.textContent = "";
+    
+    for(i=1; i<questionArray.length; i++) {
+        var questionText = document.createTextNode("Question: " + questionArray[i].Q)
+        questionDiv.appendChild(questionText)
+        var a1Text = document.createTextNode("a1: " + questionArray[i].A1);
+        a1Div.appendChild(a1Text);
+        var a2Text = document.createTextNode("a2: " + questionArray[i].A2);
+        a2Div.appendChild(a2Text);
+        var a3Text = document.createTextNode("a3: " + questionArray[i].A3);
+        a3Div.appendChild(a3Text);
+        var a4Text = document.createTextNode("a4: " + questionArray[i].A4);
+        a4Div.appendChild(a4Text);
+    }
+})
+
+
 
 // function to display correct/not correct message 
 
